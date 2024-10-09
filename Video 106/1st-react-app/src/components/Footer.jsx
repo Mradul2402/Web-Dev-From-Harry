dd9ec240042ab159b7694c4ac9bccc88e39a45e6
Mradul2402 <mradul.golu.agrahari.2003@gmail.com>
2024-10-09 10:37:57 +0530

    first commit

---
+++ Video 106/1st-react-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Copyright &copy;Mradul Agrahari | all rights reserved
+    </div>
+  )
+}
+
+export default Footer
